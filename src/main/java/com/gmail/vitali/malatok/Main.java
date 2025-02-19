293299f3756c03ff37ea16f7e862f3c71ecc73bd
VitaliiMalatok <palinamalatokpolish@gmail.com>
2025-02-19 22:27:54 +0100

    add task_1 to practical_task_l3

--- src/main/java/com/gmail/vitali/malatok/Main.java
+++ src/main/java/com/gmail/vitali/malatok/Main.java
@@ -2,6 +2,14 @@ package com.gmail.vitali.malatok;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //task_1
+        printThreeWords("Orange", "Banana", "Apple");
+    }
+
+    //task_1
+    public static void printThreeWords(String firstWord, String secondWorld, String thirdWord){
+        System.out.println(firstWord);
+        System.out.println(secondWorld);
+        System.out.println(thirdWord);
     }
 }
