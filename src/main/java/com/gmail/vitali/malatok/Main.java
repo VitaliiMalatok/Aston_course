34ab348bbce2e8914ffe4cb1e743c2c94c3b17c1
VitaliiMalatok <palinamalatokpolish@gmail.com>
2025-02-19 22:50:31 +0100

    add task_2 to practical_task_l3

--- src/main/java/com/gmail/vitali/malatok/Main.java
+++ src/main/java/com/gmail/vitali/malatok/Main.java
@@ -4,12 +4,26 @@ public class Main {
     public static void main(String[] args) {
         //task_1
         printThreeWords("Orange", "Banana", "Apple");
+        //task_2
+        checkSumSign();
     }
 
     //task_1
-    public static void printThreeWords(String firstWord, String secondWorld, String thirdWord){
+    private static void printThreeWords(String firstWord, String secondWorld, String thirdWord){
         System.out.println(firstWord);
         System.out.println(secondWorld);
         System.out.println(thirdWord);
+    }
+
+    //task_2
+    private static void checkSumSign() {
+        int a = 1;
+        int b = -1;
+        int sum = a + b;
+        if(sum >= 0){
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
     }
 }
