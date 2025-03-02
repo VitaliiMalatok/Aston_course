dd015ec3429827fe4bb805bdae04a18630e597be
VitaliiMalatok <palinamalatokpolish@gmail.com>
2025-03-02 12:34:23 +0100

    create class Animal

---
+++ src/main/java/com/gmail/vitali/malatok/Animal.java
@@ -0,0 +1,19 @@
+package com.gmail.vitali.malatok;
+
+public class Animal {
+    private final String name;
+    private static int animalCount = 0;
+
+    public Animal(String name) {
+        this.name = name;
+        animalCount++;
+    }
+
+    public void run(int distance) {
+        System.out.println(name + " ran " + distance + " m.");
+    }
+
+    public void swim(int distance) {
+        System.out.println(name + " swam " + distance + " m.");
+    }
+}
