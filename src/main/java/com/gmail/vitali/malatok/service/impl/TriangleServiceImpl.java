62b1df46139d35c2cdcc84b187a71bc6a60e428d
VitaliiMalatok <palinamalatokpolish@gmail.com>
2025-03-03 18:03:24 +0100

    Override basic method in TriangleServiceImpl

--- src/main/java/com/gmail/vitali/malatok/service/impl/TriangleServiceImpl.java
+++ src/main/java/com/gmail/vitali/malatok/service/impl/TriangleServiceImpl.java
@@ -3,18 +3,36 @@ package com.gmail.vitali.malatok.service.impl;
 import com.gmail.vitali.malatok.service.ShapeService;
 
 public class TriangleServiceImpl implements ShapeService {
+    private double sideA, sideB, sideC;
+    private String fillColor;
+    private String borderColor;
+
+    public TriangleServiceImpl(double sideA, double sideB, double sideC, String fillColor, String borderColor) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+        this.fillColor = fillColor;
+        this.borderColor = borderColor;
+    }
+
     @Override
     public double getArea() {
-        return 0;
+        double s = (sideA + sideB + sideC) / 2;
+        return Math.sqrt(s * (s - sideA) * (s - sideB) * (s - sideC));
     }
 
     @Override
     public double getPerimeter() {
-        return ShapeService.super.getPerimeter();
+        return sideA + sideB + sideC;
     }
 
     @Override
     public void printInfo() {
-        ShapeService.super.printInfo();
+        System.out.println("Triangle - Sides: " + sideA + ", " + sideB + ", " + sideC);
+        System.out.println("Area: " + getArea());
+        System.out.println("Perimeter: " + getPerimeter());
+        System.out.println("Fill Color: " + fillColor);
+        System.out.println("Border Color: " + borderColor);
+        System.out.println("---------------------------");
     }
 }
