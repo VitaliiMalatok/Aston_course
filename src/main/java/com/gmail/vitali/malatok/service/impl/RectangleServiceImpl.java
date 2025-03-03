6a6e730a1e91634a4ef81ea7cbfa23fe3ef524f5
VitaliiMalatok <palinamalatokpolish@gmail.com>
2025-03-03 17:59:21 +0100

    Override basic method in RectangleServiceImpl

--- src/main/java/com/gmail/vitali/malatok/service/impl/RectangleServiceImpl.java
+++ src/main/java/com/gmail/vitali/malatok/service/impl/RectangleServiceImpl.java
@@ -3,18 +3,34 @@ package com.gmail.vitali.malatok.service.impl;
 import com.gmail.vitali.malatok.service.ShapeService;
 
 public class RectangleServiceImpl implements ShapeService {
+    private double width, height;
+    private String fillColor;
+    private String borderColor;
+
+    public RectangleServiceImpl(double width, double height, String fillColor, String borderColor) {
+        this.width = width;
+        this.height = height;
+        this.fillColor = fillColor;
+        this.borderColor = borderColor;
+    }
+
     @Override
     public double getArea() {
-        return 0;
+        return width * height;
     }
 
     @Override
     public double getPerimeter() {
-        return ShapeService.super.getPerimeter();
+        return 2 * (width + height);
     }
 
     @Override
     public void printInfo() {
-        ShapeService.super.printInfo();
+        System.out.println("Rectangle - Width: " + width + ", Height: " + height);
+        System.out.println("Area: " + getArea());
+        System.out.println("Perimeter: " + getPerimeter());
+        System.out.println("Fill Color: " + fillColor);
+        System.out.println("Border Color: " + borderColor);
+        System.out.println("---------------------------");
     }
 }
