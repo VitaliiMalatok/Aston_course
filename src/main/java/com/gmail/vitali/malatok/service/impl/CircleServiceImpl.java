c88ffa7c9c56370241b758f498754463be249f48
VitaliiMalatok <palinamalatokpolish@gmail.com>
2025-03-03 17:56:28 +0100

    Override basic method in CircleServiceImpl

--- src/main/java/com/gmail/vitali/malatok/service/impl/CircleServiceImpl.java
+++ src/main/java/com/gmail/vitali/malatok/service/impl/CircleServiceImpl.java
@@ -3,18 +3,33 @@ package com.gmail.vitali.malatok.service.impl;
 import com.gmail.vitali.malatok.service.ShapeService;
 
 public class CircleServiceImpl implements ShapeService {
+    private double radius;
+    private String fillColor;
+    private String borderColor;
+
+    public CircleServiceImpl(double radius, String fillColor, String borderColor) {
+        this.radius = radius;
+        this.fillColor = fillColor;
+        this.borderColor = borderColor;
+    }
+
     @Override
     public double getArea() {
-        return 0;
+        return Math.PI * radius * radius;
     }
 
     @Override
     public double getPerimeter() {
-        return ShapeService.super.getPerimeter();
+        return 2 * Math.PI * radius;
     }
 
     @Override
     public void printInfo() {
-        ShapeService.super.printInfo();
+        System.out.println("Circle - Radius: " + radius);
+        System.out.println("Area: " + getArea());
+        System.out.println("Perimeter: " + getPerimeter());
+        System.out.println("Fill Color: " + fillColor);
+        System.out.println("Border Color: " + borderColor);
+        System.out.println("---------------------------");
     }
 }
