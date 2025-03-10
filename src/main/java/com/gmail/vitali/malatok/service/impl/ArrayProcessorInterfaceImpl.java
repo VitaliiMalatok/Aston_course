7862a212b439aaf16249c1502e6877d4bdaa2d9e
VitaliiMalatok <palinamalatokpolish@gmail.com>
2025-03-10 17:18:55 +0100

    create interface ArrayProcessorInterface

---
+++ src/main/java/com/gmail/vitali/malatok/service/impl/ArrayProcessorInterfaceImpl.java
@@ -0,0 +1,10 @@
+package com.gmail.vitali.malatok.service.impl;
+
+import com.gmail.vitali.malatok.service.ArrayProcessorInterface;
+
+public class ArrayProcessorInterfaceImpl implements ArrayProcessorInterface {
+    @Override
+    public void processAndPrintResults() {
+
+    }
+}
