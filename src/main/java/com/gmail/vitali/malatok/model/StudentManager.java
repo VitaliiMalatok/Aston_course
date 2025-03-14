f03142c6684f1ac8fb3bf24fa7dca05288a0296e
VitaliiMalatok <palinamalatokpolish@gmail.com>
2025-03-14 17:03:16 +0100

    create class StudentManager

---
+++ src/main/java/com/gmail/vitali/malatok/model/StudentManager.java
@@ -0,0 +1,30 @@
+package com.gmail.vitali.malatok.model;
+
+import java.util.List;
+
+public class StudentManager {
+    public static void removeLowPerformingStudents(List<Student> students) {
+        for (int i = 0; i < students.size(); i++) {
+            if (students.get(i).getAverageGrade() < 3) {
+                students.remove(i);
+                i--;
+            }
+        }
+    }
+
+    public static void promoteStudents(List<Student> students) {
+        for (Student student : students) {
+            if (student.getAverageGrade() >= 3) {
+                student.course++;
+            }
+        }
+    }
+
+    public static void printStudents(List<Student> students, int course) {
+        for (Student student : students) {
+            if (student.course == course) {
+                System.out.println(student.name);
+            }
+        }
+    }
+}
