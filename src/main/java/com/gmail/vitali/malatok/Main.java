96a4f9ba1d391aeb96173de72563103c229d63c6
VitaliiMalatok <palinamalatokpolish@gmail.com>
2025-03-24 13:34:18 +0100

    create basic project arhitect

--- src/main/java/com/gmail/vitali/malatok/Main.java
+++ src/main/java/com/gmail/vitali/malatok/Main.java
@@ -1,7 +1,11 @@
 package com.gmail.vitali.malatok;
 
+import com.gmail.vitali.malatok.service.Impl.RunTaskServiceImpl;
+import com.gmail.vitali.malatok.service.RunTaskService;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        RunTaskService runTaskService = new RunTaskServiceImpl();
+        runTaskService.runTask();
     }
 }
