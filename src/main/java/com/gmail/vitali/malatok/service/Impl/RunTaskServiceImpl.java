c15424647106ecc1856109869ba0de7d9fe7f3c5
VitaliiMalatok <palinamalatokpolish@gmail.com>
2025-03-24 15:22:49 +0100

    create basic structure of project

---
+++ src/main/java/com/gmail/vitali/malatok/service/Impl/RunTaskServiceImpl.java
@@ -0,0 +1,10 @@
+package com.gmail.vitali.malatok.service.Impl;
+
+import com.gmail.vitali.malatok.service.RunTaskService;
+
+public class RunTaskServiceImpl implements RunTaskService {
+    @Override
+    public void runTask() {
+
+    }
+}
