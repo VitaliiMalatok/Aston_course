debb15ffd2d3f183f9473e4c08e8b5742e95e9b5
VitaliiMalatok <palinamalatokpolish@gmail.com>
2025-04-04 23:03:01 +0200

    create TelegramBot class

---
+++ src/main/java/service/TelegramBot.java
@@ -0,0 +1,36 @@
+package service;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class TelegramBot extends TelegramLongPollingBot {
+    private final String token = "7904768280:AAHFkKLGinGFpm-0IvbIdSCO8rPcZdjndeY";
+    private final String chatId = "646030568";
+
+    @Override
+    public String getBotUsername() {
+        return "@Vitali_Malatok_Allure_Report_Bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return token;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+    }
+
+    public void sendMessageToTelegram(String messageText) {
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId); // Устанавливаем ID чата
+        message.setText(messageText); // Устанавливаем текст сообщения
+
+        try {
+            execute(message);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
