5e2eff18e6ce18c78afcc4555e65018dcc55e503
VitaliiMalatok <palinamalatokpolish@gmail.com>
2025-04-04 23:13:01 +0200

    create TestReporter and TestFailureHandler classes

--- src/test/java/TelegramTest.java
+++ src/test/java/TelegramTest.java
@@ -4,5 +4,6 @@ public class TelegramTest {
     public static void main(String[] args) {
         TelegramBot bot = new TelegramBot();
         bot.sendMessageToTelegram("Test message from Java");
+        // если всё настроено верно, данный скрипт отправит сообщение телеграм боту
     }
 }
